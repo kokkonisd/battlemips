70ccf16c5aa39a8400ada7818ebc93be781b7b7b
Dimitris Kokkonis <kokkonisd@gmail.com>
2018-01-09 10:42:50 +0100

    Add macros in affichage

--- MIPS/affichage.asm
+++ MIPS/affichage.asm
@@ -1,4 +1,35 @@
 .data
+# macros
+# macro pour le prologue (avec sauvegarde des variables t)
+.macro pro_t
+addiu $sp, $sp -40 #PRO on ajuste $sp
+sw $ra, 0($sp)     #PRO on sauvegarde $ra
+sw $fp, 4($sp)     #PRO on sauvegarde $fp
+sw $t0, 8($sp)     #PRO on sauvegarde t0
+sw $t1, 12($sp)    #PRO on sauvegarde t1
+sw $t2, 16($sp)    #PRO on sauvegarde t2
+sw $t3, 20($sp)    #PRO on sauvegarde t3
+sw $t4, 24($sp)    #PRO on sauvegarde t4
+sw $t5, 28($sp)    #PRO on sauvegarde t5
+sw $t6, 32($sp)    #PRO on sauvegarde t6
+sw $t7, 36($sp)    #PRO on sauvegarde t7
+addiu $fp, $sp, 40 #PRO on ajuste $fp
+.end_macro
+# macro pour l'epilogue (avec chargement des variables t)
+.macro epi_t
+lw $ra, 0($sp)     #EPI on charge $ra
+lw $fp, 4($sp)     #EPI on charge $fp
+lw $t0, 8($sp)     #EPI on charge t0
+lw $t1, 12($sp)    #EPI on charge t1
+lw $t2, 16($sp)    #EPI on charge t2
+lw $t3, 20($sp)    #EPI on charge t3
+lw $t4, 24($sp)    #EPI on charge t4
+lw $t5, 28($sp)    #EPI on charge t5
+lw $t6, 32($sp)    #EPI on charge t6
+lw $t7, 36($sp)    #EPI on charge t7
+addiu $sp, $sp, 40 #EPI on ajuste $sp
+.end_macro
+
 # variables globaux, déclarées ici temporairement
 LIGS: .word 10
 COLS: .word 10
@@ -24,18 +55,7 @@ chaineBarriere: .asciiz "  |  "
 j main		 
 
 fctAfficherCase: # ARGUMENTS : a0=contenu, a1=joueur
-	addiu $sp, $sp -40 #PRO on ajuste $sp
-    sw $ra, 0($sp)     #PRO on sauvegarde $ra
-    sw $fp, 4($sp)     #PRO on sauvegarde $fp
-    sw $t0, 8($sp)     #PRO on sauvegarde t0
-    sw $t1, 12($sp)    #PRO on sauvegarde t1
-    sw $t2, 16($sp)    #PRO on sauvegarde t2
-    sw $t3, 20($sp)    #PRO on sauvegarde t3
-    sw $t4, 24($sp)    #PRO on sauvegarde t4
-    sw $t5, 28($sp)    #PRO on sauvegarde t5
-    sw $t6, 32($sp)    #PRO on sauvegarde t6
-    sw $t7, 36($sp)    #PRO on sauvegarde t7
-    addiu $fp, $sp, 40 #PRO on ajuste $fp
+	pro_t
 	
 	ori $v0, $0, 4
 	ori $t0, $0, 3
@@ -71,35 +91,13 @@ fctAfficherCase: # ARGUMENTS : a0=contenu, a1=joueur
 		syscall
 		j switch_suite
 	switch_suite:
-	
-		lw $ra, 0($sp)     #EPI on charge $ra
-        lw $fp, 4($sp)     #EPI on charge $fp
-        lw $t0, 8($sp)     #EPI on charge t0
-        lw $t1, 12($sp)    #EPI on charge t1
-        lw $t2, 16($sp)    #EPI on charge t2
-        lw $t3, 20($sp)    #EPI on charge t3
-        lw $t4, 24($sp)    #EPI on charge t4
-        lw $t5, 28($sp)    #EPI on charge t5
-        lw $t6, 32($sp)    #EPI on charge t6
-        lw $t7, 36($sp)    #EPI on charge t7
-        addiu $sp, $sp, 40 #EPI on ajuste $sp
+		epi_t
 	
 		jr $ra
 		
 		
 fctAfficherJeu: # ARGUMENTS : -
-	addiu $sp, $sp -40 #PRO on ajuste $sp
-    sw $ra, 0($sp)     #PRO on sauvegarde $ra
-    sw $fp, 4($sp)     #PRO on sauvegarde $fp
-    sw $t0, 8($sp)     #PRO on sauvegarde t0
-    sw $t1, 12($sp)    #PRO on sauvegarde t1
-    sw $t2, 16($sp)    #PRO on sauvegarde t2
-    sw $t3, 20($sp)    #PRO on sauvegarde t3
-    sw $t4, 24($sp)    #PRO on sauvegarde t4
-    sw $t5, 28($sp)    #PRO on sauvegarde t5
-    sw $t6, 32($sp)    #PRO on sauvegarde t6
-    sw $t7, 36($sp)    #PRO on sauvegarde t7
-    addiu $fp, $sp, 40 #PRO on ajuste $fp
+	pro_t
 	
 	# afficher l'entête des grilles
 	la $a0, chaineAffichage1
@@ -247,17 +245,7 @@ fctAfficherJeu: # ARGUMENTS : -
 	ori $v0, $0, 4
 	syscall
 	
-	lw $ra, 0($sp)     #EPI on charge $ra
-    lw $fp, 4($sp)     #EPI on charge $fp
-    lw $t0, 8($sp)     #EPI on charge t0
-    lw $t1, 12($sp)    #EPI on charge t1
-    lw $t2, 16($sp)    #EPI on charge t2
-    lw $t3, 20($sp)    #EPI on charge t3
-    lw $t4, 24($sp)    #EPI on charge t4
-    lw $t5, 28($sp)    #EPI on charge t5
-    lw $t6, 32($sp)    #EPI on charge t6
-    lw $t7, 36($sp)    #EPI on charge t7
-    addiu $sp, $sp, 40 #EPI on ajuste $sp
+	epi_t
 	
 	jr $ra
 		
